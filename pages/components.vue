cabbdd76f130cddab2aacc54ea12f290d50f7c5d
Yury Feschenko <ronerycamel@gmail.com>
2022-08-25 22:34:21 +0300

    Display & spacing utilities

--- pages/components.vue
+++ pages/components.vue
@@ -1,37 +1,26 @@
 <template>
   <div class="container">
-    <div class="row-cols-7">
-      <div class="col-lg-2">
-        <UiButton block> Test button </UiButton>
+    <div class="row">
+      <div class="col-12 col-md-6 order-lg-3">
+        <div class="block">1</div>
       </div>
-      <div class="col-lg-2">
-        <UiButton variant="primary" size="lg" block> Test LG button </UiButton>
+      <div class="col-6 col-md-4 col-lg-2 order-lg-2 order-xl-none">
+        <div class="block">2</div>
       </div>
-      <div class="col-lg-2">
-        <UiButton variant="link"> Test link button </UiButton>
+      <div class="col-auto col-md-2 order-lg-1 order-xl-none">
+        <div class="block">3</div>
       </div>
-      <div class="col-lg-2 col-start-lg-1">
-        <UiButton v-for="variant in variants" :key="variant" :variant="variant" block>
-          {{ variant }}
-        </UiButton>
+      <div class="col-sm-6 col-md-8 col-lg-auto col-offset-6 col-offset-lg-8 col-offset-xl-0 order-lg-4">
+        <div class="block">4</div>
       </div>
-      <div class="col-lg-2">
-        <UiButton v-for="variant in variants" :key="variant" :variant="`${variant}-muted`" block>
-          {{ variant }}-muted
-        </UiButton>
-      </div>
-      <div class="col-lg-2">
-        <UiButton v-for="variant in variants" :key="variant" :variant="`${variant}-outline`" block>
-          {{ variant }}-outline
-        </UiButton>
-      </div>
+    </div>
+    <div class="d-flex px-40 py-24">
+      <p class="mb-0 mx-lg-auto">spacing test 2</p>
     </div>
   </div>
 </template>
 
-<script setup>
-const variants = ['primary', 'secondary', 'tertiary', 'neutral', 'neutral-variant', 'danger', 'success']
-</script>
+<script setup></script>
 
 <style lang="scss" scoped>
 .container {
@@ -45,5 +34,13 @@ const variants = ['primary', 'secondary', 'tertiary', 'neutral', 'neutral-varian
   background-position: left center;
   background-repeat: repeat-x;
   background-size: calc((100%) / var(--grid-columns)) 100%;
+}
+
+.block {
+  height: 200px;
+  margin-bottom: 1rem;
+  padding: 2rem;
+  text-align: center;
+  background-color: var(--primary-container);
 }
 </style>
