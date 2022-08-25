45ffcf7c011a35f41ab76dbc6f3543d55397ae4d
Yury Feschenko <ronerycamel@gmail.com>
2022-08-25 14:35:42 +0300

    Layout grid styles

--- pages/components.vue
+++ pages/components.vue
@@ -1,26 +1,45 @@
 <template>
-  <div>
-    <UiDatepicker v-model="date" locale="ru" />
-    <p>{{ date }}</p>
-    <UiDropdown v-model="dropdownVisible">
-      <template #toggle="{ hide, show, toggle }">
-        <UiButton @click="hide"> Hide dropdown </UiButton>
-        <UiButton @click="show"> Show dropdown </UiButton>
-        <UiButton @click="toggle"> Toggle dropdown </UiButton>
-      </template>
-      <p style="height: 200px; margin: 0; background: pink">dropdown content</p>
-    </UiDropdown>
-
-    <UiDropdown icon="close-24" text="Dropdown with props" variant="warning" block icon-right>
-      <p style="height: 200px; margin: 0; background: cyan">dropdown content</p>
-    </UiDropdown>
-
-    <p>This is text under dropdown. It should not move.</p>
+  <div class="container" style="--grid-color: rgba(255, 0, 0, 0.15); --grid-columns: 12">
+    <div class="row">
+      <div class="col-12 col-md-6">
+        <h1>col1</h1>
+      </div>
+      <div class="col-12 col-md-3 col-start-md-9">
+        <h1>col2</h1>
+      </div>
+      <div class="col-12 col-md-8">
+        <h1>col3</h1>
+      </div>
+    </div>
+  </div>
+  <div class="container" style="--grid-color: rgba(0, 255, 0, 0.15); --grid-columns: 7">
+    <div class="row-cols-7">
+      <div class="col-1">
+        <h1>column with overflowing text</h1>
+      </div>
+      <div class="col-7 col-lg-2">
+        <h1>col2</h1>
+      </div>
+      <div class="col-7 col-xxl-5">
+        <h1>col3</h1>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup>
-const dropdownVisible = ref(false)
+<script setup></script>
 
-const date = ref(new Date(2022, 7, 25))
-</script>
+<style lang="scss" scoped>
+.container {
+  background-image: linear-gradient(
+    to right,
+    transparent 12px,
+    var(--grid-color) 12px,
+    var(--grid-color) calc(100% - 12px),
+    transparent calc(100% - 12px)
+  );
+  background-position: left center;
+  background-repeat: repeat-x;
+  background-size: calc((100%) / var(--grid-columns)) 100%;
+}
+</style>
