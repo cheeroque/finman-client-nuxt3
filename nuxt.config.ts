bc181cfe15b0724daa42242fb96ffa42ecc2c590
Yury Feschenko <ronerycamel@gmail.com>
2022-10-22 23:10:59 +0300

    Added icons & manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,45 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Finance Manager 3',
+      htmlAttrs: {
+        lang: 'ru',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        {
+          hid: 'mobile-web-app-capable',
+          name: 'mobile-web-app-capable',
+          content: 'yes',
+        },
+      ],
+      link: [
+        {
+          rel: 'shortcut icon',
+          hid: 'shortcut-icon',
+          type: 'image/png',
+          href: '/icon-32.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          hid: 'apple-touch-icon',
+          type: 'image/png',
+          href: '/icon.png',
+        },
+        {
+          rel: 'manifest',
+          hid: 'manifest',
+          href: '/manifest.json',
+          crossorigin: 'use-credentials',
+        },
+      ],
+    },
+  },
+
   buildModules: ['@pinia/nuxt', 'nuxt-icons'],
 
   css: ['@/assets/styles/app.scss'],
