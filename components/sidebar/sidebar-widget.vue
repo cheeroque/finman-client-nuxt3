c4cd68da609fae6c2a08b5e551845129d208dbb3
Yury Feschenko <ronerycamel@gmail.com>
2022-10-22 16:29:06 +0300

    Added card styles

--- components/sidebar/sidebar-widget.vue
+++ components/sidebar/sidebar-widget.vue
@@ -1,34 +1,13 @@
 <template>
-  <div class="sidebar-widget">
-    <div class="widget-header">
-      <h5 class="widget-title">{{ title }}</h5>
+  <div class="card">
+    <div class="card-header">
+      <h5 class="card-title text-center">{{ title }}</h5>
     </div>
-    <div class="widget-body">
+    <div class="card-body">
       <slot />
     </div>
   </div>
 </template>
-
-<style lang="scss" scoped>
-.sidebar-widget {
-  border-radius: $card-border-radius;
-  color: $card-color;
-  background-color: $card-bg;
-}
-
-.widget-header {
-  padding: $card-padding-y $card-padding-x 0;
-}
-
-.widget-body {
-  padding: $card-padding-y $card-padding-x;
-}
-
-.widget-title {
-  margin-bottom: 0;
-  text-align: center;
-}
-</style>
 
 <script lang="ts" setup>
 const props = defineProps<{
