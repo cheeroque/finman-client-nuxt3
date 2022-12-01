9e0e0cc29939137166727913f255ee3a3ba9217d
Yury Feschenko <ronerycamel@gmail.com>
2022-12-01 20:52:30 +0300

    Handle invalid view modes

--- pages/view/[view].vue
+++ pages/view/[view].vue
@@ -1,3 +1,12 @@
 <template>
   <PageRecords />
 </template>
+
+<script setup lang="ts">
+const route = useRoute()
+const viewMode = route.params.view as string
+
+if (!['expense', 'income'].includes(viewMode)) {
+  showError({ statusCode: 404 })
+}
+</script>
