85a2d03cdc4d525b782be7b9641e3d461c46e267
Yury Feschenko <ronerycamel@gmail.com>
2022-12-24 11:10:14 +0300

    feat: spinner size prop

--- components/ui/ui-spinner.vue
+++ components/ui/ui-spinner.vue
@@ -2,6 +2,7 @@
   <span
     :aria-label="useString('loading')"
     :class="variant ? `spinner-${variant}` : null"
+    :style="{ '--spinner-size': parsedSize }"
     class="spinner"
     role="status"
   />
@@ -9,6 +10,13 @@
 
 <script lang="ts" setup>
 const props = defineProps<{
+  size?: number | string
   variant?: string
 }>()
+
+const parsedSize = computed(() => {
+  if (!props.size) return
+  const isNumber = typeof props.size === 'number' || `${props.size}` === `${parseInt(props.size || '')}`
+  return isNumber ? `${props.size}px` : props.size
+})
 </script>
