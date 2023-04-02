32d232b63c8fc1be50f44cc94a68ae9fb733f693
Yury Feschenko <ronerycamel@gmail.com>
2023-04-02 21:22:37 +0300

    fix: display sidebar calendar on all pages

--- components/sidebar/sidebar-calendar.vue
+++ components/sidebar/sidebar-calendar.vue
@@ -1,13 +1,25 @@
 <template>
   <SidebarWidget :title="useString('calendar')">
-    <MonthCalendar :date="startDate" />
+    <MonthCalendar :date="activeDate" />
   </SidebarWidget>
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{
-  startDate?: Date
-}>()
+const route = useRoute()
+
+const activeDate = computed(() => {
+  let date
+
+  const routeDate = route.params.month as string
+
+  if (routeDate) {
+    const routeYear = Number(routeDate.split('-')[0])
+    const routeMonth = Number(routeDate.split('-')[1])
+    date = new Date(routeYear, routeMonth - 1)
+  }
+
+  return date
+})
 </script>
 
 <style lang="scss" scoped>
--- components/sidebar/sidebar.vue
+++ components/sidebar/sidebar.vue
@@ -3,25 +3,10 @@
     <SearchForm class="mb-24" />
     <SidebarMonthly class="mb-24" />
     <Transition name="fade" mode="out-in">
-      <SidebarCalendar v-if="showCalendar" :start-date="startDate" />
+      <SidebarCalendar />
     </Transition>
   </aside>
 </template>
-
-<script setup lang="ts">
-const route = useRoute()
-const showCalendar = computed(() => route.path !== '/months')
-const startDate = computed(() => {
-  let date
-  const routeDate = route.params.month as string
-  if (routeDate) {
-    const routeYear = Number(routeDate.split('-')[0])
-    const routeMonth = Number(routeDate.split('-')[1])
-    date = new Date(routeYear, routeMonth - 1)
-  }
-  return date
-})
-</script>
 
 <style lang="scss" scoped>
 .sidebar {
