9a1e30b8ba9f9cb402900741665f9298f3a2c0e1
Yury Feschenko <ronerycamel@gmail.com>
2023-04-29 17:32:42 +0300

    fix: useContrastColor with 3-character hex values

--- composables/useContrastColor.ts
+++ composables/useContrastColor.ts
@@ -1,17 +1,24 @@
-export function useContrastColor(
-  hex: string = '',
-  defaultDark: string = '#000',
-  defaultLight: string = '#fff'
-): string {
-  if (!hex?.length) return defaultDark
+export function useContrastColor(hex: string = '', dark: string = '#000', light: string = '#fff'): string {
+  if (!hex.length) return dark
 
-  const color = hex.substring(1)
   const rgb = []
   let lightness = 255
+
+  let color = hex.substring(1)
+
+  /* Normalize color from #XXX to #XXXXXX */
+  if (color.length === 3) {
+    color = color
+      .split('')
+      .map((ch) => `${ch}${ch}`)
+      .join('')
+  }
+
   for (let i = 0; i < 3; i++) {
     rgb[i] = parseInt(color.substring(i * 2, i * 2 + 2), 16)
   }
+
   lightness = 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2]
 
-  return lightness >= 165 ? defaultDark : defaultLight
+  return lightness >= 165 ? dark : light
 }
