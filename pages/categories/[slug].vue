1b9a3a1bcddb20d58832092cb363e25f505e5573
Yury Feschenko <ronerycamel@gmail.com>
2023-04-30 12:04:54 +0300

    feat: fetch month records with GQL

--- pages/categories/[slug].vue
+++ pages/categories/[slug].vue
@@ -1,86 +1,133 @@
 <template>
-  <PageContent :loading="pending" :title="category.name" spinner-variant="primary" class="overflow-hidden">
-    <GroupTable :group-label="useString('date')" :items="tableItems" :loading="pending" />
+  <PageContent :loading="recordsStore.loading" :title="category.name" spinner-variant="primary" class="overflow-hidden">
+    <GroupTable :group-label="useString('date')" :items="tableItems" :loading="recordsStore.loading" />
+
     <template #footer v-if="totalPages > 1">
-      <UiPagination :disabled="pending" :total-pages="totalPages" hide-prev-next />
+      <UiPagination :disabled="recordsStore.loading" :total-pages="totalPages" hide-prev-next />
     </template>
   </PageContent>
 </template>
 
 <script setup lang="ts">
 import { DateTime } from 'luxon'
-import { RecordsCategory, RecordsItem } from '~~/types/records'
+import { RecordsItem } from '~~/types/records'
 import { useRecordsStore } from '~/store/records'
 
-type CategoryRecordsByMonth = {
-  [key: string]: RecordsItem[]
+import RECORDS_QUERY from '@/graphql/Records.gql'
+
+interface CategoryRecordsByMonth {
+  [key: string]: TableItem
 }
 
-type CategoryRecordsResponse = {
-  category?: RecordsCategory
-  data: CategoryRecordsByMonth
-  total: number
+interface RecordsQueryResponse {
+  records: RecordsQueryResponseRecords
 }
 
-type CategoryRecordsRequestParams = {
-  id?: number
-  order?: string
-  orderBy?: string
-  page?: number
-  perPage?: number
+interface RecordsQueryResponseRecords {
+  data: RecordsItem[]
+  paginatorInfo: PaginatorInfo
+}
+
+interface TableItem {
+  group: string
+  records: RecordsItem[]
+  subtotal: number
+  timestamp: number
 }
 
 const route = useRoute()
 const recordsStore = useRecordsStore()
 const category = recordsStore.categories.find(({ slug }) => slug === route.params.slug)
+
 if (!category) {
   throw createError({ statusMessage: 'Not found', statusCode: 404 })
 }
 
-const { data, pending, refresh } = await useAsyncData(
-  `categories/${route.params.slug}/${route.query.page}`,
-  async () => {
-    const headers = useRequestHeaders(['cookie'])
-    const cookie = headers.cookie as string
+const perPage = ref<number>()
+const page = ref<number>()
+const tableItems = ref<TableItem[]>([])
 
-    const perPage = Number(route.query.perPage) || 12
-    const query: CategoryRecordsRequestParams = {
-      id: category?.id,
-      order: (route.query.order as string) || 'DESC',
-      orderBy: (route.query.orderBy as string) || 'created_at',
-      page: Number(route.query.page) || 1,
-      perPage,
-    }
-    const fetchOptions = { method: 'GET', headers: { cookie }, query }
+const totalPages = computed<number>(() => {
+  if (!recordsStore.firstRecord?.created_at) return 1
 
-    const { data, total } = await $fetch<CategoryRecordsResponse>('/api/data/category/records', fetchOptions)
+  /* Get total count of months between now and first record, then divide it
+   * by months per page */
+  const startDate = DateTime.fromFormat(recordsStore.firstRecord.created_at, 'yyyy-LL-dd HH:mm:ss')
+  const months = Math.ceil(Math.abs(startDate.diffNow('month').months))
 
-    const tableItems = Object.entries(data)
-      .map(([key, value]) => {
-        const date = DateTime.fromFormat(key, 'yyyy-LL')
-        const timestamp = date.valueOf()
-        const group = date.toLocaleString({ month: 'short', year: 'numeric' }, { locale: useLocale() })
-        const subtotal = value?.reduce((total, { sum }) => (total += sum), 0)
+  return Math.ceil(months / (perPage.value ?? 18))
+})
 
-        return { group, timestamp, subtotal, records: value }
-      })
-      .sort(({ timestamp: a }, { timestamp: b }) => b - a)
+async function fetchRecords() {
+  perPage.value = Number(route.query.perPage) || 18
+  page.value = Number(route.query.page) || 1
 
-    const totalPages = Math.ceil(total / perPage)
+  const now = DateTime.now()
 
-    return { tableItems, totalPages }
+  const to = now
+    .set({ hour: 0, minute: 0, second: 0, day: 1 })
+    .plus({ month: 1 })
+    .minus({ month: (page.value - 1) * perPage.value, second: 1 })
+
+  const from = to.minus({ month: perPage.value - 1 }).set({ hour: 0, minute: 0, second: 0, day: 1 })
+
+  const variables = {
+    where: {
+      AND: [
+        { column: 'CREATED_AT', operator: 'GTE', value: from.toFormat('yyyy-LL-dd HH:mm:ss') },
+        { column: 'CREATED_AT', operator: 'LTE', value: to.toFormat('yyyy-LL-dd HH:mm:ss') },
+      ],
+    },
   }
-)
 
-const tableItems = computed(() => data?.value?.tableItems || [])
-const totalPages = computed(() => data?.value?.totalPages || 0)
+  recordsStore.pending++
+
+  try {
+    const { data, error } = await useAsyncQuery<RecordsQueryResponse>(RECORDS_QUERY, variables)
+
+    if (error.value) throw error.value
+
+    if (data.value?.records?.data) {
+      /* Group records by month */
+      const monthGroups: CategoryRecordsByMonth = {}
+
+      data.value.records.data.forEach((record) => {
+        const month = DateTime.fromFormat(record.created_at, 'yyyy-LL-dd HH:mm:ss').set({
+          hour: 0,
+          minute: 0,
+          second: 0,
+          day: 1,
+        })
+
+        const key = month.toFormat('yyyy-LL')
+        const group = month.toLocaleString({ month: 'long', year: 'numeric' }, { locale: useLocale() })
+        const timestamp = month.valueOf()
+
+        let subtotal = record.sum
+
+        if (!monthGroups[key]) {
+          monthGroups[key] = { group, records: [record], subtotal, timestamp }
+        } else {
+          subtotal += monthGroups[key].records.reduce((acc, cur) => (acc += cur.sum), 0)
+          monthGroups[key].subtotal = subtotal
+          monthGroups[key].records.push(record)
+        }
+      })
+
+      tableItems.value = Object.values(monthGroups)
+    }
+  } catch (error) {}
+
+  recordsStore.pending--
+}
 
 watch(
   () => route.query,
   async () => {
-    await refresh()
+    await fetchRecords()
     setTimeout(() => useScrollTo('.page'), 250)
-  }
+  },
+  { immediate: true }
 )
 </script>
 
