a90f86321d2bb6a170f8345e8b780ba774296d3c
Yury Feschenko <ronerycamel@gmail.com>
2023-05-05 11:19:21 +0300

    refactor: removed old data fetching endpoints

--- composables/useApiFetch.ts
+++
@@ -1,6 +0,0 @@
-import { FetchOptions } from 'ofetch'
-
-export function useApiFetch(url: string, options?: FetchOptions) {
-  const headers = useRequestHeaders(['cookie']) as HeadersInit
-  return $fetch(url, { ...options, headers })
-}
