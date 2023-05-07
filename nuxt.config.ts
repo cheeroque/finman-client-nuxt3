603cf3ec7881915fcd914dde985843e0a80c4262
Yury Feschenko <ronerycamel@gmail.com>
2023-05-07 13:07:13 +0300

    fix: removed svg2img fix from config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,5 @@
 import { handleNuxtReady } from './hooks/ready'
 
-// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
-if (
-  process.env.LD_LIBRARY_PATH == null ||
-  !process.env.LD_LIBRARY_PATH.includes(`${process.env.PWD}/node_modules/canvas/build/Release:`)
-) {
-  process.env.LD_LIBRARY_PATH = `${process.env.PWD}/node_modules/canvas/build/Release:${
-    process.env.LD_LIBRARY_PATH || ''
-  }`
-}
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: false,
