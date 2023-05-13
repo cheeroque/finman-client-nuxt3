f7d46cc401ae05b5220b7d14311e0a4727f209e9
cheeroque <ronerycamel@gmail.com>
2023-05-13 16:24:57 +0300

    fix: disabled useNuxtApp in auth midware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -5,11 +5,11 @@ export default defineNuxtRouteMiddleware(async (to) => {
   /* Guard only pages that are not public */
   if (!to.meta.isPublic) {
     const authStore = useAuthStore()
-    const { $auth } = useNuxtApp()
+    // const { $auth } = useNuxtApp()
 
     if (!authStore.token) {
       try {
-        await $auth.fetchUser()
+        // await $auth.fetchUser()
       } catch (error: any) {
         if (error instanceof AuthError) {
           // $auth.reset()
