3205f03f4b5fecd5ee5f30c0ba8464e14e88dca1
yu.feshenko <yu.feshenko@ekf.su>
2023-11-13 17:34:40 +0300

    refactor: search page

--- pages/search.vue
+++ pages/search.vue
@@ -1,86 +1,62 @@
 <template>
   <PageContent
-    :loading="recordsStore.loading"
+    :loading="fetching"
     :title="useString('searchResults', searchQuery)"
-    spinner-variant="primary"
     class="overflow-hidden"
+    spinner-variant="primary"
   >
     <RecordTable :records="tableItems" />
 
     <template #footer>
       <div v-if="totalPages > 1">
-        <UiPagination :disabled="recordsStore.loading" :total-pages="totalPages" hide-prev-next />
+        <UiPagination :disabled="fetching" :total-pages="totalPages" hide-prev-next />
       </div>
     </template>
   </PageContent>
 </template>
 
 <script setup lang="ts">
-import { useRecordsStore } from '~/store/records'
+import { useQuery } from '@urql/vue'
 
 import RECORDS_QUERY from '~/graphql/Records.gql'
 
-import type { RecordsItem, RecordsQueryResponse, RecordsQueryVariables } from '~/types/records'
-
 const route = useRoute()
-const recordsStore = useRecordsStore()
 
 const searchQuery = computed(() => route.query.q as string)
+const perPage = computed(() => Number(route.query.perPage) || 50)
 
-const tableItems = ref<RecordsItem[]>([])
-
-const totalPages = ref(1)
-
-async function fetchSearchResults() {
-  /** Build query variables */
-  const column = (route.query.orderBy as string) ?? 'CREATED_AT'
-  const order = (route.query.order as string) ?? 'DESC'
-  const first = Number(route.query.perPage) || 50
+const variables = computed(() => {
+  const column = String(route.query.orderBy ?? 'CREATED_AT')
+  const order = String(route.query.order ?? 'DESC')
   const page = Number(route.query.page) || 1
 
   /* Find records by note */
+
   const where = {
     OR: [{ column: 'NOTE', operator: 'LIKE', value: `%${searchQuery.value}%` }],
   }
 
-  /* Also find records by exact sum if q can be cast to number */
+  /* Also find records by exact sum if search query can be cast to number */
+
   if (!isNaN(Number(searchQuery.value))) {
     where.OR.push({ column: 'SUM', operator: 'EQ', value: searchQuery.value })
   }
 
-  const variables: RecordsQueryVariables = {
-    first,
+  return {
+    first: perPage.value,
     orderBy: [{ column, order }],
     page,
     where,
   }
+})
 
-  try {
-    const { data, error } = await useAsyncQuery<RecordsQueryResponse>(RECORDS_QUERY, variables)
+const { data: result, fetching } = useQuery({
+  query: RECORDS_QUERY,
+  variables,
+})
 
-    if (error.value) throw error.value
-
-    if (data.value?.records?.data) {
-      tableItems.value = data.value.records.data
-      totalPages.value = data.value.records.paginatorInfo.lastPage
-    }
-  } catch (error) {}
-}
-
-watch(
-  () => route.query,
-  async () => {
-    await fetchSearchResults()
-    setTimeout(() => {
-      /* If window is scrolled down (e.g. in mobile) scroll it back to top,
-       * otherwise scroll back page element */
-      const windowTop = useWindowTop()
-      const target = !windowTop ? '.page' : undefined
-      useScrollTo(target)
-    }, 250)
-  },
-  { immediate: true }
-)
+const tableItems = computed(() => result.value?.records?.data ?? [])
+const totalPages = computed(() => Math.ceil(result.value?.records?.paginatorInfo?.total ?? 0 / perPage.value))
 </script>
 
 <style lang="scss" scoped>
