3a361ee3557400ba52cbc3cc49aa5cca8f8cf86e
yu.feshenko <yu.feshenko@ekf.su>
2023-12-18 17:21:32 +0300

    feat: add basic bar chart

---
+++ components/chart/ChartBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <ChartWrapper>
+    <div ref="chart" class="chart-container" />
+  </ChartWrapper>
+</template>
+
+<script setup lang="ts">
+import 'chartist/dist/index.css'
+import { BarChart } from 'chartist'
+
+import type { BarChartData, BarChartOptions } from 'chartist'
+
+interface ChartBarProps {
+  data: BarChartData
+  options?: BarChartOptions
+}
+
+const props = defineProps<ChartBarProps>()
+
+const chart = ref()
+
+onMounted(() => {
+  if (chart.value instanceof HTMLElement) {
+    new BarChart(chart.value, props.data, props.options)
+  }
+})
+</script>
---
+++ components/chart/ChartWrapper.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="chart-wrapper">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.chart-wrapper {
+  position: relative;
+  width: 100%;
+
+  &::before {
+    display: block;
+    content: '';
+    padding-bottom: 56.25%;
+  }
+
+  :deep(.chart-container) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100% !important;
+    height: 100% !important;
+  }
+}
+
+@include media-max-width(md) {
+  .chart-wrapper {
+    &::before {
+      padding-bottom: 80%;
+    }
+  }
+}
+</style>
