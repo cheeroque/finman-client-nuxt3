6552f2728e4db227cbe1369efcbed1a9ffd8daaa
yu.feshenko <yu.feshenko@ekf.su>
2023-12-27 15:23:42 +0300

    feat: custom chart bar rendering

--- components/chart/ChartBar.vue
+++ components/chart/ChartBar.vue
@@ -1,44 +1,87 @@
 <template>
   <ChartWrapper>
-    <div ref="chart" :style="{ '--ct-bar-width': barWidth }" class="chart-container" />
+    <div ref="chartEl" class="chart-container" />
   </ChartWrapper>
 </template>
 
 <script setup lang="ts">
-import { BarChart } from 'chartist'
+import { BarChart, Svg } from 'chartist'
 
-import type { BarChartData, BarChartOptions } from 'chartist'
+import type { BarChartData, BarChartOptions, BarDrawEvent, DrawEvent } from 'chartist'
 
 interface ChartBarProps {
+  barBorderRadius?: number | string
+  barRatio?: number
   data: BarChartData
+  labelFormatter?: (value?: number) => string
   options?: BarChartOptions
 }
 
-const props = defineProps<ChartBarProps>()
-
-const chart = ref()
-const barWidth = ref('3rem')
-
-onMounted(() => {
-  if (chart.value instanceof HTMLElement) {
-    new BarChart(chart.value, props.data, props.options)
-  }
-
-  handleResize()
-
-  window.addEventListener('resize', handleResize, { passive: true })
+const props = withDefaults(defineProps<ChartBarProps>(), {
+  barBorderRadius: '0.25rem',
+  barRatio: 0.7,
 })
 
-function handleResize() {
-  if (chart.value instanceof HTMLElement) {
-    const gridEl = chart.value.querySelector('.ct-grids')
-    const boundary = gridEl ?? chart.value
+const chartEl = ref()
+const chartInstance = ref()
 
-    const { width } = boundary.getBoundingClientRect()
+const barCount = computed(() => props.data?.labels?.length ?? 1)
 
-    const barCount = Number(props.data?.labels?.length) || 1
+onMounted(() => {
+  if (chartEl.value instanceof HTMLElement) {
+    chartInstance.value = new BarChart(chartEl.value, props.data, props.options)
 
-    barWidth.value = `${Math.floor((width * 0.7) / barCount)}px`
+    chartInstance.value.on('draw', (data: DrawEvent) => {
+      if (isBarData(data)) {
+        const rect = getBarRect(data)
+        const label = getBarLabel(data)
+
+        data.element.replace(rect)
+        data.group.append(label)
+      }
+    })
   }
+})
+
+function getBarLabel(data: BarDrawEvent) {
+  let value
+
+  const x = data.x1 - getBarWidth(data) / 2
+  const y = data.y2
+
+  if (typeof data.value === 'number') {
+    value = data.value
+  } else if ('y' in data.value) {
+    value = data.value.y
+  }
+
+  const textEl = new Svg('text', { x, y }, 'ct-bar-label')
+  const text = typeof props.labelFormatter === 'function' ? props.labelFormatter(value) : String(value)
+
+  textEl.text(text)
+
+  return textEl
+}
+
+function getBarRect(data: BarDrawEvent) {
+  const width = getBarWidth(data)
+  const height = Math.abs(data.y1 - data.y2)
+
+  const x = data.x1 - width / 2
+  const y = data.y2
+
+  const rx = props.barBorderRadius
+
+  return new Svg('rect', { x, y, width, height, rx, 'data-variant': 'primary' }, 'ct-bar')
+}
+
+function getBarWidth(data: BarDrawEvent) {
+  const chartWidth = data.chartRect.width()
+
+  return Math.floor((chartWidth * props.barRatio * 0.5) / barCount.value) * 2
+}
+
+function isBarData(data: DrawEvent): data is BarDrawEvent {
+  return data.type === 'bar'
 }
 </script>
--- components/chart/ChartWrapper.vue
+++ components/chart/ChartWrapper.vue
@@ -21,6 +21,9 @@
     left: 0;
     width: 100% !important;
     height: 100% !important;
+    padding: $card-padding-y $card-padding-x;
+    border-radius: $card-border-radius;
+    background-color: $card-bg;
   }
 }
 
@@ -41,18 +44,23 @@ $ct-text-line-height: 1.5;
 $ct-grid-color: var(--neutral-outline);
 $ct-grid-dasharray: 0;
 
-$ct-bar-width: var(--ct-bar-width, 2rem);
-
-$ct-series-names: (a, b, c, d, e, f, g);
-$ct-series-colors: (
-  var(--primary),
-  var(--secondary),
-  var(--tertiary),
-  var(--neutral),
-  var(--neutral-variant),
-  var(--danger),
-  var(--success)
-);
+$ct-bar-width: 0;
 
 @import 'chartist/dist/index.scss';
+
+.ct-bar {
+  fill: var(--ct-bar-fill, var(--primary));
+}
+
+.ct-bar-label {
+  font-family: $font-family-alternate;
+  font-size: 0.875rem;
+  fill: var(--neutral);
+  transform: translateY(-0.5rem);
+}
+
+.ct-label {
+  font-family: $font-family-alternate;
+  font-size: 0.8125rem;
+}
 </style>
