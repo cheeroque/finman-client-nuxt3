fa33a875932d68de054c253b4eff59690935f631
Yury Feschenko <yu.feshenko@ekf.su>
2024-01-20 12:52:41 +0300

    build: add graphql codegen

---
+++ codegen.ts
@@ -0,0 +1,21 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+import loader from './codegen-loader'
+
+const config: CodegenConfig = {
+  // @ts-ignore as using filename here is broken due to bug in graphql-code-generator
+  schema: [{ api: { loader } }],
+
+  documents: 'gql/**/*.ts',
+  ignoreNoDocuments: true,
+  require: ['dotenv/config', 'ts-node/register'],
+  generates: {
+    'gen/gql/': {
+      preset: 'client',
+      config: {
+        useTypeImports: true,
+      },
+    },
+  },
+}
+
+export default config
