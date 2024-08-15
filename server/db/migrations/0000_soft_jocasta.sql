f3ab3b5421a329f72c6d7a5eac5dc03d9e607c0d
Yury Feschenko <yu.feshenko@ekf.su>
2024-08-15 11:43:48 +0300

    build: users & sessions migrations

---
+++ server/db/migrations/0000_soft_jocasta.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"password" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
