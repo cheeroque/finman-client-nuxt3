1c92439a8164ff2917d6ff468d734a8007df01d8
Yury Feschenko <yu.feshenko@ekf.su>
2024-08-15 13:02:03 +0300

    fix: pass connection string to pg client in drizzle util

--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -2,8 +2,11 @@ import { drizzle, type NodePgDatabase } from 'drizzle-orm/node-postgres'
 import pg from 'pg'
 import * as schema from '../db/schema'
 
-export async function useDrizzle() {
-  const client = new pg.Client()
+export async function getDrizzle() {
+  const client = new pg.Client({
+    connectionString: process.env.NUXT_POSTGRES_URL,
+  })
+
   await client.connect()
 
   client.on('error', (error) => {
