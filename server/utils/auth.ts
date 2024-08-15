33176642b2a928dbc9aac1437584dccc00e53c6b
Yury Feschenko <yu.feshenko@ekf.su>
2024-08-15 13:02:49 +0300

    fix: lucia server util

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -1,28 +1,31 @@
 import { Lucia } from 'lucia'
 import { DrizzlePostgreSQLAdapter } from '@lucia-auth/adapter-drizzle'
 import { SessionsTable, UsersTable } from '../db/schema'
-import { useDrizzle } from './drizzle'
+import { getDrizzle } from './drizzle'
 
-const db = await useDrizzle()
-const adapter = new DrizzlePostgreSQLAdapter(db, SessionsTable, UsersTable)
+export async function getLucia() {
+  const db = await getDrizzle()
+  const adapter = new DrizzlePostgreSQLAdapter(db, SessionsTable, UsersTable)
 
-export const lucia = new Lucia(adapter, {
-  sessionCookie: {
-    attributes: {
-      secure: !import.meta.dev,
+  return new Lucia(adapter, {
+    sessionCookie: {
+      attributes: {
+        secure: !import.meta.dev,
+      },
     },
-  },
-  getUserAttributes: (attributes) => ({ username: attributes.username }),
-})
+    getUserAttributes: (attributes) => ({ username: attributes.name }),
+  })
+}
 
 declare module 'lucia' {
   interface Register {
-    Lucia: typeof lucia
+    Lucia: typeof Lucia
     DatabaseUserAttributes: DatabaseUserAttributes
     UserId: number
   }
 }
 
 interface DatabaseUserAttributes {
-  username: string
+  email: string
+  name: string
 }
