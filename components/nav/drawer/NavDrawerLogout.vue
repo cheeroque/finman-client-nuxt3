1f9d0621510b622cd38a2e46ee1ca06e02fc074f
Yury Feschenko <yu.feshenko@ekf.su>
2024-08-18 12:04:14 +0300

    feat: rewrite logout

--- components/nav/drawer/NavDrawerLogout.vue
+++ components/nav/drawer/NavDrawerLogout.vue
@@ -1,16 +1,18 @@
 <template>
-  <UiButton class="drawer-item" icon="logout-24" icon-size="24" @click="logout">
+  <UiButton :loading="loading" class="drawer-item" icon="logout-24" icon-size="24" @click="logout">
     <span class="caption">{{ useString('logout') }}</span>
   </UiButton>
 </template>
 
 <script setup lang="ts">
-const user = useSession()
+const loading = ref(false)
 
 async function logout() {
+  loading.value = true
+
   await $fetch('/api/logout', { method: 'POST' })
 
-  user.value = undefined
+  loading.value = false
 
   return navigateTo('/login', { external: true })
 }
--- server/api/logout.post.ts
+++ server/api/logout.post.ts
@@ -1,20 +1,13 @@
-import { LogoutMutation } from '~/graphql'
-
 export default defineEventHandler(async (event) => {
-  const { client, headers } = event.context
-
-  const { data, error } = await client.mutation(LogoutMutation, {}, { fetchOptions: { headers } }).toPromise()
-
-  /* Remove auth token client-side cookie by passing `set-cookie` header
-   * with expiration date in the past */
-
-  setResponseHeader(event, 'set-cookie', [
-    `auth_token=deleted; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/; Secure; HttpOnly`,
-  ])
-
-  if (error) {
-    throw error
+  if (!event.context.session) {
+    throw createError({
+      statusCode: 403,
+    })
   }
 
-  return { data }
+  const lucia = await getLucia()
+
+  await lucia.invalidateSession(event.context.session.id)
+
+  appendHeader(event, 'Set-Cookie', lucia.createBlankSessionCookie().serialize())
 })
